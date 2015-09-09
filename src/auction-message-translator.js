28f7aee2914dadf0c3bdd792106647e9194ac420
Shai Yallin <shai.yallin@gmail.com>
2015-09-09 20:52:56 +0300

    end of chapter 12

--- src/auction-message-translator.js
+++ src/auction-message-translator.js
@@ -2,7 +2,8 @@ export default function AuctionMessageTranslator(listener) {
 
     return {
         processMessage: function (topic, message) {
-            if (message.command == 'Close') listener.auctionClosed();
+            if (message.command === 'Close') listener.auctionClosed()
+            else if (message.command === 'Price') listener.currentPrice(message.currentPrice, message.increment);
         }
     }
 }
--- test/auction-message-translator.spec.js
+++ test/auction-message-translator.spec.js
@@ -8,13 +8,23 @@ chai.use(SinonChai);
 
 describe("an auction message translator", () => {
     it("notifies the auction when a close message has been received", () => {
-        const auctionClosedListener = sinon.spy();
-        const translator = new AuctionMessageTranslator({auctionClosed: auctionClosedListener});
+        const auctionClosed = sinon.spy();
+        const translator = new AuctionMessageTranslator({auctionClosed});
         const message = {command: "Close"};
 
         translator.processMessage(null, message);
 
-        expect(auctionClosedListener).to.have.been.called;
+        expect(auctionClosed).to.have.been.called;
+    });
+
+    it("notifies bid details when current price message received", () => {
+        const currentPrice = sinon.spy();
+        const translator = new AuctionMessageTranslator({currentPrice});
+        const message = {command: "Price", currentPrice: 192, increment: 7, bidder: "Someone else"}
+
+        translator.processMessage(null, message);
+
+        expect(currentPrice).to.have.been.calledWith(192, 7);
     });
 });
 
